895da5ec252fcb26d230694f1449a626442ff770
Kirti Goel <kirtigoyal5104@gmail.com>
2024-11-05 12:11:47 +0530

    added basic project 1

---
+++ 01project/src/App.js
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+      <Chai/>
+      <h1> Hello World! | KIRTI GOYAL
+      <p>
+        learned basics about React
+      </p>
+      </h1>
+    </>
+  );
+}
+
+export default App;
